a7f771d60914a980fe32ccdd6b6c385c10eafb1d
Sergio_ORC <sergiooctavioruiz@gmail.com>
2026-05-27 00:54:48 -0600

    refactor: implement functions for RPG character validation

--- Python_basics/03-Build_an_RPG_Character.py
+++ Python_basics/03-Build_an_RPG_Character.py
@@ -1,9 +1,8 @@
-# First try
+# Second try
 full_dot = '●'
 empty_dot = '○'
 
-def create_character(name, strength, intelligence, charisma):
-    # Name validation
+def name_validation(name):
     if not isinstance(name, str):
         return 'The character name should be a string'
     if not name:
@@ -12,7 +11,8 @@ def create_character(name, strength, intelligence, charisma):
         return 'The character name is too long'
     if ' ' in name:
         return 'The character name should not contain spaces'
-    # Stats validation
+
+def stats_validation(strength, intelligence, charisma):
     if not isinstance(strength, int) or not isinstance(intelligence, int) or not isinstance(charisma, int):
         return 'All stats should be integers'
     if strength < 1 or intelligence < 1 or charisma < 1:
@@ -21,28 +21,21 @@ def create_character(name, strength, intelligence, charisma):
         return 'All stats should be no more than 4'
     if strength + intelligence + charisma != 7:
         return 'The character should start with 7 points'
+
+def stats_points(stat):
+    return full_dot*stat + empty_dot*(10-stat)
+
+def create_character(name, strength, intelligence, charisma):
+    name_error = name_validation(name)
+    if name_error:
+        return name_error
+    stats_error = stats_validation(strength, intelligence, charisma)
+    if stats_error:
+        return stats_error
     
-    # Return string creation
-    strength_points = ''
-    intelligence_points = ''
-    charisma_points = ''
-    while True:
-        if len(strength_points) < strength:
-            strength_points += '●'
-        elif len(strength_points) < 10:
-            strength_points += '○'
-        if len(intelligence_points) < intelligence:
-            intelligence_points += '●'
-        elif len(intelligence_points) < 10:
-            intelligence_points += '○'
-        if len(charisma_points) < charisma:
-            charisma_points += '●'
-        elif len(charisma_points) < 10:
-            charisma_points += '○'
-        
-        if len(strength_points) == 10 and len(intelligence_points) == 10 and len(charisma_points) == 10:
-            break
-    
-    return f'{name}\nSTR {strength_points}\nINT {intelligence_points}\n CHA {charisma_points}'
+    return (f'{name}\n'
+            f'STR {stats_points(strength)}\n'
+            f'INT {stats_points(intelligence)}\n'
+            f'CHA {stats_points(charisma)}\n')
 
 print(create_character('ren', 4, 2, 1))
