ce656a7bc290518b4b166d1311fd194495a49505
Sergio_ORC <sergiooctavioruiz@gmail.com>
2026-05-28 20:30:20 -0600

    refactor: separation and reorganization of validation logic

--- Python_basics/02-Build_an_Apply_Discount_Function.py
+++ Python_basics/02-Build_an_Apply_Discount_Function.py
@@ -1,22 +1,27 @@
-def apply_discount(price, discount):
-    if (type(price) == int or type(price) == float) and (type(discount) == int or type(discount) == float):
-        if price <= 0 and (discount > 0 or discount <= 100):
-            return 'The price should be greater than 0'
-        elif price > 0 and (discount < 0 or discount > 100):
-            return 'The discount should be between 0 and 100'
-        elif price <= 0 and (discount < 0 or discount > 100):
-            return 'The price should be greater than 0', 'The discount should be between 0 and 100'
-        else:
-            final_price = price - (price * discount / 100)
-            print(final_price)
-            return final_price
-    if (type(price) == int or type(price) == float) and (type(discount) != int or type(discount) != float):
-        return 'The discount should be a number'
-    if (type(price) != int or type(price) != float) and (type(discount) == int or type(discount) == float):
-        return 'The price should be a number'
-    else:
-        return 'The discount should be a number', 'The price should be a number'
+def data_validation(price, discount):
+    error_messages = []
 
+    if not isinstance(price, (int, float)):
+        error_messages.append('The price should be a number')
+    elif price <= 0:
+        error_messages.append('The price should be greater than 0')
+
+    if not isinstance(discount, (int, float)):
+        error_messages.append('The discount should be a number')
+    elif not (0 <= discount <= 100):
+        error_messages.append('The discount should be between 0 and 100')
+
+    if error_messages:
+        return error_messages
+
+def apply_discount(price, discount):
+
+    error_messages = data_validation(price, discount)
+    if error_messages:
+        return error_messages
+    
+    final_price = price * (1 - discount / 100)
+    return final_price
 
 price = 100
 discount = 20
